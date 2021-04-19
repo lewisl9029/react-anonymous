fac0fed105784eaf1e0224706e305ee1fd163e6e
Lewis Liu <lewisl9029@users.noreply.github.com>
2021-04-18 21:16:20 -0700

    Fix missed rename

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export const Boundary = ({ children }) => children()
-export const anonymous = (children, { key } = {}) => React.createElement(Boundary, { children, key })
+export const Anonymous = ({ children }) => children()
+export const anonymous = (children, { key } = {}) => React.createElement(Anonymous, { children, key })
 
 export default anonymous
